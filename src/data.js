8c203e31146418ab241b20bcb4f445ac1ebb2e7d
Adriana <nossa.musica.07.10@gmail.com>
2018-07-15 17:19:14 -0500

    El login tan esperado

--- src/data.js
+++ src/data.js
@@ -96,38 +96,6 @@ window.computeStudentsStats = (laboratoria) => {
   return studentArr;
 };
 
-window.computeGenerationsStats = (laboratoria) => {
-  const generationsArray = []; // se crea un array vacío
-  const obj = { // se pide un objeto con las siguientes propiedades
-    campus: '',
-    generation: '',
-    average: 0,
-    count: 0,
-  };
-  // Se crea la variable para almacenar el promedio de avance por generación
-  let average = 0;
-  // se
-  for (key in laboratoria) {
-    obj.campus = key;
-    average = 0;
-    const generations = Object.keys(laboratoria.key.generacion);
-    generations.forEach((generation) => {
-      obj.generation = generation;
-      const students = laboratoria.key.generacion.generation.estudiantes;
-      for (student in students) {
-        average += students.student.progreso.porcentajeCompletado;
-        average = average / students.length;
-        obj.average = average;
-        obj.count = students.length;
-        generationsArray.push(obj);
-      }
-    });
-  };
-  return generationsArray;
-};
-
-window.sortStudents = (students, orderBy, orderDirection) => {};
-
 window.filterStudents = (students, search) => {
   let searchResult = [];
   students.forEach(resElement => {
